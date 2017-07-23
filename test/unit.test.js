b85e2bb7af78efa6f8b2442f2f5a4daa691db93d
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-07-23 16:30:37 -0400

    resolve path in utimes

--- test/unit.test.js
+++ test/unit.test.js
@@ -327,7 +327,7 @@ test('Call refreshFiles when dependency is modified', async t => {
 
   // eslint-disable-next-line no-magic-numbers
   await sleep(150);
-  utimes(partial, Date.now(), Date.now());
+  utimes(path.resolve(partial), Date.now(), Date.now());
   t.is(path.resolve(partial), await change);
   t.true(info.firstCall.calledWith(match('Changed file'), path.resolve(partial)));
   t.true(info.calledOnce);
