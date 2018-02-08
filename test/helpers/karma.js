e862b4b56be87e5c63ad94252bb873a8ab23cde9
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-07 19:00:14 -0500

    test: increase timeout for run to complete

--- test/helpers/karma.js
+++ test/helpers/karma.js
@@ -112,7 +112,7 @@ export async function waitForRunComplete(server) {
   try {
     const [, result] = await pEvent(server, 'run_complete', {
       multiArgs: true,
-      timeout: 10000,
+      timeout: 30000,
       rejectionEvents: ['browser_error'],
     });
 
