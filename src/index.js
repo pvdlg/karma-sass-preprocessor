776504d2aaf677de44158bd5ae17c9596fdba872
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-15 10:35:58 -0500

    style: lint

--- src/index.js
+++ src/index.js
@@ -14,111 +14,111 @@ import sass from 'node-sass';
  * @return {Function} the function to preprocess files.
  */
 function createSassPreprocessor(args, config, logger, server) {
-  const preprocessorConfig = config.sassPreprocessor || {};
-  const log = logger.create('preprocessor.sass');
-  const options = merge({sourceMap: false}, args.options || {}, preprocessorConfig.options || {});
-  const transformPath =
-    args.transformPath ||
-    preprocessorConfig.transformPath ||
-    (filepath => `${path.dirname(filepath)}/${path.basename(filepath, path.extname(filepath))}.css`);
-  let watcher;
-  const dependencies = {};
-  const unlinked = [];
+	const preprocessorConfig = config.sassPreprocessor || {};
+	const log = logger.create('preprocessor.sass');
+	const options = merge({sourceMap: false}, args.options || {}, preprocessorConfig.options || {});
+	const transformPath =
+		args.transformPath ||
+		preprocessorConfig.transformPath ||
+		(filepath => `${path.dirname(filepath)}/${path.basename(filepath, path.extname(filepath))}.css`);
+	let watcher;
+	const dependencies = {};
+	const unlinked = [];
 
-  if (config.autoWatch) {
-    watcher = new FSWatcher({persistent: true, disableGlobbing: true})
-      .on('change', filePath => {
-        log.info('Changed file "%s".', filePath);
-        server.refreshFiles();
-      })
-      .on('add', filePath => {
-        if (unlinked.indexOf(filePath) !== -1) {
-          log.info('Added file "%s".', filePath);
-          server.refreshFiles();
-        }
-      })
-      .on('unlink', filePath => {
-        log.info('Deleted file "%s".', filePath);
-        unlinked.push(filePath);
-        server.refreshFiles();
-      });
-  }
+	if (config.autoWatch) {
+		watcher = new FSWatcher({persistent: true, disableGlobbing: true})
+			.on('change', filePath => {
+				log.info('Changed file "%s".', filePath);
+				server.refreshFiles();
+			})
+			.on('add', filePath => {
+				if (unlinked.indexOf(filePath) !== -1) {
+					log.info('Added file "%s".', filePath);
+					server.refreshFiles();
+				}
+			})
+			.on('unlink', filePath => {
+				log.info('Deleted file "%s".', filePath);
+				unlinked.push(filePath);
+				server.refreshFiles();
+			});
+	}
 
-  return (content, file, done) => {
-    log.debug('Processing "%s".', file.originalPath);
-    file.path = transformPath(file.originalPath);
-    // Clone the options because we need to mutate them
-    const opts = Object.assign({}, options);
+	return (content, file, done) => {
+		log.debug('Processing "%s".', file.originalPath);
+		file.path = transformPath(file.originalPath);
+		// Clone the options because we need to mutate them
+		const opts = Object.assign({}, options);
 
-    // Inline source maps
-    if (opts.sourceMap || opts.map) {
-      opts.sourceMap = true;
-      opts.sourceMapEmbed = true;
-    }
-    opts.indentedSyntax = file.originalPath.indexOf('.sass') !== -1;
-    opts.file = file.originalPath;
-    opts.outFile = file.originalPath;
+		// Inline source maps
+		if (opts.sourceMap || opts.map) {
+			opts.sourceMap = true;
+			opts.sourceMapEmbed = true;
+		}
+		opts.indentedSyntax = file.originalPath.indexOf('.sass') !== -1;
+		opts.file = file.originalPath;
+		opts.outFile = file.originalPath;
 
-    sass.render(opts, (err, result) => {
-      if (err) {
-        log.error('%s\n  at %s:%d', err.message, file.originalPath, err.line);
-        return done(err, null);
-      }
-      if (
-        config.autoWatch &&
-        config.files.find(
-          configFile => configFile.watched && minimatch(file.originalPath, configFile.pattern, {dot: true})
-        )
-      ) {
-        const fullPath = path.resolve(file.originalPath);
-        const includedFiles = [];
-        const startWatching = [];
-        const stopWatching = [];
+		sass.render(opts, (err, result) => {
+			if (err) {
+				log.error('%s\n  at %s:%d', err.message, file.originalPath, err.line);
+				return done(err, null);
+			}
+			if (
+				config.autoWatch &&
+				config.files.find(
+					configFile => configFile.watched && minimatch(file.originalPath, configFile.pattern, {dot: true})
+				)
+			) {
+				const fullPath = path.resolve(file.originalPath);
+				const includedFiles = [];
+				const startWatching = [];
+				const stopWatching = [];
 
-        for (let i = 0, {length} = result.stats.includedFiles; i < length; i++) {
-          const includedFile = path.resolve(result.stats.includedFiles[i]);
+				for (let i = 0, {length} = result.stats.includedFiles; i < length; i++) {
+					const includedFile = path.resolve(result.stats.includedFiles[i]);
 
-          if (includedFile !== fullPath) {
-            includedFiles.push(includedFile);
-            if (!dependencies[includedFile]) {
-              startWatching.push(includedFile);
-              log.debug('Watching "%s"', includedFile);
-              dependencies[includedFile] = [fullPath];
-            } else if (dependencies[includedFile].indexOf(fullPath) === -1) {
-              dependencies[includedFile].push(fullPath);
-            }
-          }
-        }
+					if (includedFile !== fullPath) {
+						includedFiles.push(includedFile);
+						if (!dependencies[includedFile]) {
+							startWatching.push(includedFile);
+							log.debug('Watching "%s"', includedFile);
+							dependencies[includedFile] = [fullPath];
+						} else if (dependencies[includedFile].indexOf(fullPath) === -1) {
+							dependencies[includedFile].push(fullPath);
+						}
+					}
+				}
 
-        for (let i = 0, keys = Object.keys(dependencies), {length} = keys; i < length; i++) {
-          if (includedFiles.indexOf(keys[i]) === -1) {
-            const index = dependencies[keys[i]].indexOf(fullPath);
+				for (let i = 0, keys = Object.keys(dependencies), {length} = keys; i < length; i++) {
+					if (includedFiles.indexOf(keys[i]) === -1) {
+						const index = dependencies[keys[i]].indexOf(fullPath);
 
-            if (index !== -1) {
-              dependencies[keys[i]].splice(index, 1);
-              if (!dependencies[keys[i]].length > 0) {
-                stopWatching.push(keys[i]);
-                log.debug('Stop watching "%s"', keys[i]);
-                delete dependencies[keys[i]];
-              }
-            }
-          }
-        }
+						if (index !== -1) {
+							dependencies[keys[i]].splice(index, 1);
+							if (!dependencies[keys[i]].length > 0) {
+								stopWatching.push(keys[i]);
+								log.debug('Stop watching "%s"', keys[i]);
+								delete dependencies[keys[i]];
+							}
+						}
+					}
+				}
 
-        if (startWatching.length > 0) {
-          watcher.add(startWatching);
-        }
-        if (stopWatching.length > 0) {
-          watcher.unwatch(stopWatching);
-        }
-      }
+				if (startWatching.length > 0) {
+					watcher.add(startWatching);
+				}
+				if (stopWatching.length > 0) {
+					watcher.unwatch(stopWatching);
+				}
+			}
 
-      if (opts.sourceMap && result.map) {
-        file.sourceMap = JSON.parse(result.map);
-      }
-      return done(null, result.css);
-    });
-  };
+			if (opts.sourceMap && result.map) {
+				file.sourceMap = JSON.parse(result.map);
+			}
+			return done(null, result.css);
+		});
+	};
 }
 
 // Inject dependencies
--- test/integration.test.js
+++ test/integration.test.js
@@ -8,74 +8,74 @@ import {run, watch, waitForRunComplete} from './helpers/karma';
 let stubWrite;
 
 test.before(() => {
-  stubWrite = stub(process.stdout, 'write');
+	stubWrite = stub(process.stdout, 'write');
 });
 
 test.after(() => {
-  stubWrite.restore();
+	stubWrite.restore();
 });
 
 test.serial('Compile scss file', async t => {
-  const {success, error, disconnected, errMsg} = await run([
-    'test/fixtures/basic.scss',
-    'test/fixtures/styles.test.js',
-  ]);
+	const {success, error, disconnected, errMsg} = await run([
+		'test/fixtures/basic.scss',
+		'test/fixtures/styles.test.js',
+	]);
 
-  t.ifError(error, `Karma returned the error: ${errMsg}`);
-  t.ifError(disconnected, 'Karma disconnected');
-  t.is(success, 1, 'Expected 1 test successful');
+	t.ifError(error, `Karma returned the error: ${errMsg}`);
+	t.ifError(disconnected, 'Karma disconnected');
+	t.is(success, 1, 'Expected 1 test successful');
 });
 
 test.serial('Compile scss file with custom preprocessor', async t => {
-  const {success, error, disconnected, errMsg} = await run([
-    'test/fixtures/basic.custom.scss',
-    'test/fixtures/styles.test.js',
-  ]);
+	const {success, error, disconnected, errMsg} = await run([
+		'test/fixtures/basic.custom.scss',
+		'test/fixtures/styles.test.js',
+	]);
 
-  t.ifError(error, `Karma returned the error: ${errMsg}`);
-  t.ifError(disconnected, 'Karma disconnected');
-  t.is(success, 1, 'Expected 1 test successful');
+	t.ifError(error, `Karma returned the error: ${errMsg}`);
+	t.ifError(disconnected, 'Karma disconnected');
+	t.is(success, 1, 'Expected 1 test successful');
 });
 
 test.serial('Log error on invalid scss file', async t => {
-  const {error, disconnected, exitCode} = await run('test/fixtures/error.scss');
+	const {error, disconnected, exitCode} = await run('test/fixtures/error.scss');
 
-  t.ifError(disconnected, 'Karma disconnected');
-  t.true(error, 'Expected an error to be returned');
-  t.is(exitCode, 1, 'Expected non zero exit code');
+	t.ifError(disconnected, 'Karma disconnected');
+	t.true(error, 'Expected an error to be returned');
+	t.is(exitCode, 1, 'Expected non zero exit code');
 });
 
 test.serial('Re-compile scss file when dependency is modified', async t => {
-  const dir = tempy.directory();
-  const fixture = path.join(dir, 'with-partial.scss');
-  const includePath = path.join(dir, 'partials');
-  const partial = path.join(includePath, '_partial.scss');
-  const subPartial = path.join(includePath, '_sub-partial.scss');
+	const dir = tempy.directory();
+	const fixture = path.join(dir, 'with-partial.scss');
+	const includePath = path.join(dir, 'partials');
+	const partial = path.join(includePath, '_partial.scss');
+	const subPartial = path.join(includePath, '_sub-partial.scss');
 
-  await Promise.all([
-    copy('test/fixtures/partials/_partial.scss', partial),
-    copy('test/fixtures/partials/_sub-partial.scss', subPartial),
-    copy('test/fixtures/with-partial.scss', fixture),
-  ]);
-  const {server, watcher} = await watch(
-    [fixture.replace('fixtures', '*').replace('with', '+(with|nomatch)'), 'test/fixtures/styles.test.js'],
-    {options: {includePaths: [includePath]}}
-  );
+	await Promise.all([
+		copy('test/fixtures/partials/_partial.scss', partial),
+		copy('test/fixtures/partials/_sub-partial.scss', subPartial),
+		copy('test/fixtures/with-partial.scss', fixture),
+	]);
+	const {server, watcher} = await watch(
+		[fixture.replace('fixtures', '*').replace('with', '+(with|nomatch)'), 'test/fixtures/styles.test.js'],
+		{options: {includePaths: [includePath]}}
+	);
 
-  try {
-    let {success, error, disconnected, errMsg} = await waitForRunComplete(server);
+	try {
+		let {success, error, disconnected, errMsg} = await waitForRunComplete(server);
 
-    t.ifError(error, `Karma returned the error: ${errMsg}`);
-    t.ifError(disconnected, 'Karma disconnected');
-    t.is(success, 1, 'Expected 1 test successful');
+		t.ifError(error, `Karma returned the error: ${errMsg}`);
+		t.ifError(disconnected, 'Karma disconnected');
+		t.is(success, 1, 'Expected 1 test successful');
 
-    watcher.emit('change', partial);
-    ({success, error, disconnected, errMsg} = await waitForRunComplete(server));
+		watcher.emit('change', partial);
+		({success, error, disconnected, errMsg} = await waitForRunComplete(server));
 
-    t.ifError(error, `Karma returned the error: ${errMsg}`);
-    t.ifError(disconnected, 'Karma disconnected');
-    t.is(success, 1, 'Expected 1 test successful');
-  } finally {
-    await server.emitAsync('exit');
-  }
+		t.ifError(error, `Karma returned the error: ${errMsg}`);
+		t.ifError(disconnected, 'Karma disconnected');
+		t.is(success, 1, 'Expected 1 test successful');
+	} finally {
+		await server.emitAsync('exit');
+	}
 });
